74fd91c0a1be27204dfdde30262bde0574bd82db
Andre Pankratz <andre@webwarelab.com>
2010-09-12 12:20:19 +0200

    Omit default rails request params from #valid_request? + improved specs

--- spec/vidibus/secure/extensions/controller_spec.rb
+++ spec/vidibus/secure/extensions/controller_spec.rb
@@ -1,3 +1,4 @@
+require "ostruct"
 require "spec_helper"
 require "action_controller"
 
@@ -22,15 +23,42 @@ describe "Vidibus::Secure::Extensions::Controller" do
     end
     
     it "should build URI from request object if no :uri is provided" do
+      mock(controller.request).protocol {"http://"}
+      mock(controller.request).host_with_port {"vidibus.org"}
+      mock(controller.request).fullpath {"/"}
       controller.valid_request?(secret, :method => "get", :params => {})
     end
     
     it "should use request.method if no :method is provided" do
+      mock(controller.request).method {"get"}
       controller.valid_request?(secret, :uri => "something", :params => {})
     end
     
     it "should use request.params if no :params are provided" do
+      mock(controller.request).params {{}}
       controller.valid_request?(secret, :uri => "something", :method => "get")
+    end
+    
+    it "should use given params" do
+      dont_allow(controller).request
+      controller.valid_request?(secret, :method => "get", :uri => "something", :params => {})
+    end
+    
+    it "should return true for valid requests" do
+      Vidibus::Secure.sign_request(:get, "http://vidibus.org/", controller.request.params, secret)
+      controller.valid_request?(secret).should be_true
+    end
+    
+    it "should omit :action, :controller, and :id from request.params" do
+      Vidibus::Secure.sign_request(:get, "http://vidibus.org/", controller.request.params, secret)
+      controller.request.params.merge(:action => "index", :controller => "application", :id => nil)
+      controller.valid_request?(secret).should be_true
+    end
+    
+    it "should keep :action, :controller, and :id in custom params" do
+      params = { :action => "index", :controller => "application", :id => "12" }
+      Vidibus::Secure.sign_request(:get, "http://vidibus.org/", params, secret)
+      controller.valid_request?(secret, :params => params).should be_true
     end
     
     it "should call Vidibus::Secure.verify_request" do
