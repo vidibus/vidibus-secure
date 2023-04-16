db125d910b05258296b0ae753c0e8c6289ed6205
André Pankratz <punkrats@punkbook.local>
2023-04-15 20:24:29 -0400

    Don't create initializer for secret key

--- lib/generators/vidibus_secure_key/vidibus_secure_key_generator.rb
+++ lib/generators/vidibus_secure_key/vidibus_secure_key_generator.rb
@@ -1,11 +1,7 @@
 class VidibusSecureKeyGenerator < Rails::Generators::Base
-  desc 'Generates an initializer that sets ENV["VIDIBUS_SECURE_KEY"]'
+  desc 'Creates secure key to be stored as ENV["VIDIBUS_SECURE_KEY"]'
 
-  def create_initializer
-    create_file "config/initializers/vidibus_secure_key.rb" do
-      %(# This is a secret key for encrypting values of field defined by attr_encrypted.\n) +
-      %(# Do not change this encryption key! Otherwise you will not be able to decrypt data already stored in your database.\n) +
-      %(ENV["VIDIBUS_SECURE_KEY"] = "#{Vidibus::Secure.random(:encoding => :base64, :length => 100)}")
-    end
-  end
+  puts "\nAdd VIDIBUS_SECURE_KEY to your env file like ~/.bashrc, /etc/profile or ~/.zprofile"
+  key = %(export VIDIBUS_SECURE_KEY=#{Vidibus::Secure.random(:encoding => :base64, :length => 100)})
+  puts key
 end
